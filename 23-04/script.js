6531c41282062fce73958e961918aca4092832b7
Gustavo <gustavo96ma@gmail.com>
2026-04-23 21:06:14 -0300

    23-04

---
+++ 23-04/script.js
@@ -0,0 +1,36 @@
+console.log('Hello World!');
+
+const text = 'ALOOU, BOA NOITE!';
+
+// var numero = 2; --> É PROIBIDO USAR VAR
+let numero = 2;
+console.log(numero);
+
+numero = 10;
+console.log(numero);
+
+const decimal = 0.2;
+console.log(decimal);
+
+const booleano = true;
+console.log(booleano);
+
+let vetor = [0, 1.3, 'dois', 3, 4];
+console.log(vetor);
+
+vetor = false;
+console.log(vetor);
+
+const map = {'chave1': 1, 'chave2': 2, 'chave3': 3};
+console.log(map);
+
+const nulo = null;
+console.log(nulo);
+
+if(nulo != 2){
+    console.log('UAU!');
+} else {
+    console.log('CHIMBINHA!');
+}
+
+
